3b20582beac5fcd9b0ac5a4f573ec4d37f856627
HDLLakshan <HDLLakshan@users.noreply.github.com>
2020-04-25 01:14:07 +0530

    Add FloorAdd Functions

--- Server/src/main/Server.java
+++ Server/src/main/Server.java
@@ -1,15 +1,27 @@
 package main;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.rmi.AlreadyBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
 import java.util.Scanner;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 
@@ -57,11 +69,7 @@ public class Server extends UnicastRemoteObject implements Service {
         while(sc.hasNext()) {
             inline += sc.nextLine();
         }
-        System.out.println("JSon");
-        System.out.println(inline);
         
-        
-
         conn.disconnect();
 
       } catch (MalformedURLException e) {
@@ -78,5 +86,142 @@ public class Server extends UnicastRemoteObject implements Service {
 		
 		return inline;
 	}
+
+	@Override
+	public int getFloorCount() throws RemoteException {
+		// TODO Auto-generated method stub
+		String cnt = "";
+		int fcnt = 0;
+		try {
+			URL url = new URL("http://localhost:5000/getFloorCount");
+	        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	        conn.setRequestMethod("GET");
+	        conn.setRequestProperty("Accept", "application/json");
+
+	        if (conn.getResponseCode() != 200) {
+	            throw new RuntimeException("Failed : HTTP error code : "
+	                    + conn.getResponseCode());
+	        }
+
+	        Scanner sc = new Scanner(url.openStream());
+	        while(sc.hasNext()) {
+	            cnt += sc.nextLine();
+	        }
+	        
+	        
+	        JSONObject jb = new JSONObject(cnt); 
+              fcnt = jb.getInt("count");
+	        conn.disconnect();
+
+	      } catch (MalformedURLException e) {
+
+	        e.printStackTrace();
+
+	      } catch (IOException e) {
+
+	        e.printStackTrace();
+
+	      } catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		return fcnt;
+	}
+
+	@Override
+	public int getRoomCount(String i) throws RemoteException {
+		// TODO Auto-generated method stub
+		String cnt = "";
+		int rcnt = 0;
+		try {
+			URL url = new URL("http://localhost:5000/getRoomsCount/" + i);
+	        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	        conn.setRequestMethod("GET");
+	        conn.setRequestProperty("Accept", "application/json");
+
+	        if (conn.getResponseCode() != 200) {
+	            throw new RuntimeException("Failed : HTTP error code : "
+	                    + conn.getResponseCode());
+	        }
+
+	        Scanner sc = new Scanner(url.openStream());
+	        while(sc.hasNext()) {
+	            cnt += sc.nextLine();
+	        }
+	        
+	        
+	        JSONObject jb = new JSONObject(cnt); 
+              rcnt = jb.getInt("count");
+	        conn.disconnect();
+
+	      } catch (MalformedURLException e) {
+
+	        e.printStackTrace();
+
+	      } catch (IOException e) {
+
+	        e.printStackTrace();
+
+	      } catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return rcnt;
+	}
+
+	@Override
+	public void AddRoom(String fnum) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+		try {
+		
+			 HttpClient client = HttpClient.newHttpClient();
+		        HttpRequest request = HttpRequest.newBuilder()
+		                .uri(URI.create("http://localhost:5000/addRoom/" + fnum ))
+		                .POST(HttpRequest.BodyPublishers.ofString(""))
+		                .build();
+
+		        HttpResponse<String> response = client.send(request,
+		                HttpResponse.BodyHandlers.ofString());
+
+		        System.out.println(response.body());
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+	}
+
+	@Override
+	public void AddFloor() throws RemoteException{
+		// TODO Auto-generated method stub
+		try {
+		
+			 HttpClient client = HttpClient.newHttpClient();
+		        HttpRequest request = HttpRequest.newBuilder()
+		                .uri(URI.create("http://localhost:5000/addFloor"))
+		                .POST(HttpRequest.BodyPublishers.ofString(""))
+		                .build();
+
+		        HttpResponse<String> response = client.send(request,
+		                HttpResponse.BodyHandlers.ofString());
+
+		        System.out.println(response.body());
+			
+	       
+	        } catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	
+	
 
 }
--- Server/src/main/Service.java
+++ Server/src/main/Service.java
@@ -6,5 +6,8 @@ import java.rmi.RemoteException;
 public interface Service  extends Remote{
 	
 	public String Getdata() throws RemoteException;
-
+	public int getFloorCount() throws RemoteException;
+	public int getRoomCount(String i) throws RemoteException;
+	public void AddRoom(String fnum) throws RemoteException;
+	public void AddFloor() throws RemoteException;
 }
